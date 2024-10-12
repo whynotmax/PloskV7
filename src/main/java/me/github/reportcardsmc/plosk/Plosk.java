dc815089c4f2160b457556d7c196757befa7908b
KeineSecrets <miethe.max@web.de>
2024-10-12 14:46:30 +0200

    Fixed usage of deprecated methods in Plosk.java

--- src/main/java/me/github/reportcardsmc/plosk/Plosk.java
+++ src/main/java/me/github/reportcardsmc/plosk/Plosk.java
@@ -5,6 +5,7 @@ import ch.njol.skript.SkriptAddon;
 import com.plotsquared.core.PlotSquared;
 import org.bstats.bukkit.Metrics;
 import org.bstats.charts.SimplePie;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.IOException;
@@ -26,18 +27,18 @@ public final class Plosk extends JavaPlugin {
         plot = PlotSquared.get();
         if (plot == null) {
             getLogger().severe("You don't have plotsquared 6 installed.");
-            getPluginLoader().disablePlugin(this);
+            Bukkit.getPluginManager().disablePlugin(this);
             return;
         }
         addon = Skript.registerAddon(this);
         if (!loadClasses()) {
             getLogger().severe("Couldn't load skript classes.");
-            getPluginLoader().disablePlugin(this);
+            Bukkit.getPluginManager().disablePlugin(this);
             return;
         } else {
             getLogger().info("Classes were registered.");
         }
-        getLogger().info("Addon has been enabled");
+        getLogger().info("Addon has been enabled. Enjoy!");
     }
 
     private boolean loadClasses() {
